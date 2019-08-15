896f15d496746072a2c7a39c40084b85ffea465c
Felipe Ortiz <luigi.pipe10@outlook.com>
2019-08-15 12:13:49 -0500

    Conexion

--- ProyectoCieluz/ServidorProyecto/Datos/clConexion.cs
+++ ProyectoCieluz/ServidorProyecto/Datos/clConexion.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,38 @@ namespace ServidorProyecto.Datos
 {
     public class clConexion
     {
+        SqlConnection objConexion = null;
+        public clConexion()
+        {
+            try
+            {
+                objConexion = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\bdCieluz.mdf;Integrated Security=True");
+            }
+            catch (Exception error)
+            {
+                string salida = error.Message;
+
+            }
+        }
+
+        public int mtdConectado(string consulta)
+        {
+            objConexion.Open();
+            SqlCommand comando = new SqlCommand(consulta, objConexion);
+            int resultado = comando.ExecuteNonQuery();
+            objConexion.Close();
+            return resultado;
+        }
+
+        public DataSet mtdDesconectado(string consulta)
+        {
+            objConexion.Open();
+            SqlDataAdapter adaptador = new SqlDataAdapter(consulta, objConexion);
+            DataSet dsDatos = new DataSet();
+            adaptador.Fill(dsDatos, "tblDatos");
+            objConexion.Close();
+            return dsDatos;
+
+        }
     }
 }
