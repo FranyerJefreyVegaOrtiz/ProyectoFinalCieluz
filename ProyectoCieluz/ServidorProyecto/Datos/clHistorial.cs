38ef0125b1189bfd06c273230ae3500f1996c5a6
dasherrera <dsherrera39@misena.edu.co>
2019-08-15 14:44:31 -0500

    clHistorial

---
+++ ProyectoCieluz/ServidorProyecto/Datos/clHistorial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace ServidorProyecto.Datos
+{
+    public class clHistorial
+    {
+
+        public int IdHistorial { get; set; }
+        public int idProducto { get; set; }
+        public int idCliente { get; set; }
+
+        public DataSet mtdListar()
+        {
+            string consulta = "SELECT * FROM Historial";
+            DataSet dsHistorial = new DataSet();
+            clConexion objConexion = new clConexion();
+            dsHistorial = objConexion.mtdDesconectado(consulta);
+            return dsHistorial;
+        }
+
+        public int mtdEliminar()
+        {
+            string consulta = "Delete from Pedido where IdHistorial=" + IdHistorial + "";
+            clConexion objconexion = new clConexion();
+            int resultado = objconexion.mtdConectado(consulta);
+            return resultado;
+        }
+    }
+}
---
+++ ProyectoCieluz/ServidorProyecto/Datos/clPedido.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace ServidorProyecto.Datos
+{
+    public class clPedido
+    {
+        public int IdPedido { get; set; }
+        public int Cantidad { get; set; }
+        public int Talla { get; set; }
+        public int PrecioTotal { get; set; }
+        public float TipoPago { get; set; }
+        public int idProducto { get; set; }
+        public int idCliente { get; set; }
+        public int idAdministrador { get; set; }
+
+        public DataSet mtdListar()
+        {
+            string consulta = "Select * From Pedido";
+            DataSet dsPedido = new DataSet();
+            clConexion objConexion = new clConexion();
+            dsPedido = objConexion.mtdDesconectado(consulta);
+            return dsPedido;
+
+        }
+
+        public int mtdRegistrar(clPedido objPedido)
+        {
+            string consulta = "insert into Pedido (Cantidad, Talla, PrecioTotal, TipoPago, idProducto, idCliente, idAdministrador )" +
+               "Values('" + Cantidad + "','" + Talla + "','" + PrecioTotal + "','" + TipoPago + "', " + idProducto + ", " + idCliente + ", " + idAdministrador + ")";
+            clConexion objconexion = new clConexion();
+            int result = objconexion.mtdConectado(consulta);
+            return result;
+        }
+
+
+        public int mtdEliminar()
+        {
+            string consulta = "Delete from Pedido where IdPedido=" + IdPedido + "";
+            clConexion objconexion = new clConexion();
+            int resultado = objconexion.mtdConectado(consulta);
+            return resultado;
+        }
+    }
+}
